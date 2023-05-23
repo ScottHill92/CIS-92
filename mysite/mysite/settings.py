2152d99c93a72ceabf815064a011eea08344dfca
Scott Hill <scotthill1017@gmail.com>
2023-05-23 22:53:17 +0000

    desperate

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = [ "https://*.a.run.app" ]
+
+CSRF_TRUSTED_ORIGINS = [ "https://cis-92-vghnsd47la-uc.a.run.app" ]
 
 # Application definition
 
